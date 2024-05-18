42c052028158b1c1268c03d093ffb0a5fc61ec14
Liam Collod <lcollod@gmail.com>
2024-05-18 12:16:05 +0200

    chore(doc.execinject): improve error reporting

--- doc/source/extensions/execinject.py
+++ doc/source/extensions/execinject.py
@@ -14,6 +14,13 @@ from sphinx.application import Sphinx
 from sphinx.util.typing import ExtensionMetadata
 
 
+DIRECTIVE_NAME = "exec-inject"
+
+
+class ExecutionError(RuntimeError):
+    pass
+
+
 def execute_python_code(code: Union[str, Path]) -> str:
     command = [sys.executable]
     if isinstance(code, Path):
@@ -29,20 +36,32 @@ def execute_python_code(code: Union[str, Path]) -> str:
         encoding="utf-8",
     )
     if result.returncode != 0:
-        raise RuntimeError(
-            f"Error executing directive python code:\n"
-            f"stdout={result.stdout}\n"
-            f"sterr={result.stderr}"
+        raise ExecutionError(
+            f"# Error while executing code in subprocess:\n"
+            f"__stdout__:{result.stdout}\n"
+            f"__stderr__:{result.stderr}"
         )
 
     result_text = result.stdout or "" + result.stderr or ""
     return result_text
 
 
-def generate_error_nodes(source_file_path: Path, line: int, error: Exception):
+def generate_error_nodes(
+    source_file_path: Path,
+    line: int,
+    code_content: Optional[Union[str, Path]] = None,
+):
     nodes = [None]
     pnode = docutils.nodes.paragraph
-    nodes += [pnode(text=f"Error in {Path(source_file_path).name}:{line}: {error}")]
+    nodes += [
+        pnode(
+            text=(
+                f"With directive {DIRECTIVE_NAME} in {Path(source_file_path).name}:{line}"
+            )
+        )
+    ]
+    if code_content:
+        nodes += [docutils.nodes.literal_block(code_content, code_content)]
     traceback_txt = traceback.format_exc()
     nodes += [docutils.nodes.literal_block(traceback_txt, traceback_txt)]
     return [docutils.nodes.error(*nodes)]
@@ -78,27 +97,38 @@ class ExecDirective(Directive):
             if filepath:
                 if not filepath.is_absolute():
                     filepath = Path(source_file_path).parent / filepath
-                code_content = filepath.read_text(encoding="utf-8")
+                code_content = filepath
             else:
                 code_content = "\n".join(self.content)
 
-            text = execute_python_code(code_content)
+            try:
+                text = execute_python_code(code_content)
+            except ExecutionError as error:
+                self.reporter.error(
+                    f"{DIRECTIVE_NAME} directive failed to complete.",
+                    line=self.lineno,
+                )
+                return generate_error_nodes(source_file_path, self.lineno, code_content)
             lines = docutils.statemachine.string2lines(
                 text,
                 tab_width,
                 convert_whitespace=True,
             )
         except Exception as error:
-            return generate_error_nodes(source_file_path, self.lineno, error)
+            self.reporter.error(
+                f"{DIRECTIVE_NAME} directive failed to complete.",
+                line=self.lineno,
+            )
+            return generate_error_nodes(source_file_path, self.lineno)
 
         self.state_machine.insert_input(lines, source_file_path)
         return []
 
 
 def setup(app: Sphinx) -> ExtensionMetadata:
-    app.add_directive("exec-inject", ExecDirective)
+    app.add_directive(DIRECTIVE_NAME, ExecDirective)
     return {
-        "version": "0.1",
+        "version": "0.2",
         "parallel_read_safe": True,
         "parallel_write_safe": True,
     }
